633c1fb05bd8be4da709b2a54f772b1a5d9a26a2
MartinXPN <mirakyanmartin@gmail.com>
2021-09-02 21:42:13 +0400

    improved the compilers factory

--- checkers/compilers.py
+++ checkers/compilers.py
@@ -14,16 +14,18 @@ class Compiler(ABC):
 
     @staticmethod
     def from_language(language: str) -> 'Compiler':
-        if 'c++' in language:
+        language = language.lower()
+        if language in CppCompiler.supported_standards:
             return CppCompiler(language_standard=language)
-        if 'python' in language:
-            return PythonCompiler()
+        if language in PythonCompiler.supported_standards:
+            return PythonCompiler(language_standard=language)
         raise ValueError(f'{language} does not have a compiler yet')
 
 
 @dataclass
 class CppCompiler(Compiler):
     language_standard: str
+    supported_standards = {'c++11', 'c++14', 'c++17', 'c++20'}
 
     def compile(self, submission_path: Path):
         executable_path = submission_path.with_suffix('.o')
@@ -37,7 +39,10 @@ class CppCompiler(Compiler):
 
 @dataclass
 class PythonCompiler(Compiler):
+    language_standard: str
+    supported_standards = {'python', 'python3'}
+
     def compile(self, submission_path: Path):
-        executable_path = f'python {submission_path}'
+        executable_path = f'{self.language_standard} {submission_path}'
         print(f'Evaluating python submission with: `{executable_path}`')
         return executable_path, Stats(max_rss=0, max_vms=0, total_time=0, return_code=0, outputs='', errors='')
