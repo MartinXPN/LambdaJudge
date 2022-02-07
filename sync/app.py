d2fc23ba011f0e8fd84ea2f59080770ae12472e9
MartinXPN <mirakyanmartin@gmail.com>
2022-02-08 00:33:57 +0400

    implemented a lambda function that is triggered when an item is uploaded to an S3 bucket

---
+++ sync/app.py
@@ -0,0 +1,36 @@
+import json
+from pathlib import Path
+
+import boto3
+
+ROOT = Path('/tmp/')
+s3 = boto3.client('s3')
+secret_manager = boto3.client('secretsmanager')
+encryption_secret_key_id = 'arn:aws:secretsmanager:us-east-1:370358067229:secret:efs/problem/encryptionKey-xTnJWC'
+
+
+def sync_handler(event, context):
+    print('Event:', type(event), event)
+    print('Context:', context)
+
+    key = event['Records'][0]['s3']['object']['key']  # some-bucket/some-folder/img.jpg
+    problem = key.split('.')[0]
+    problem_file = f'/mnt/efs/{problem}.gz.fer'
+    zip_path = ROOT / f'{problem}.zip'
+    print('problem_file', problem_file, 'zip:', zip_path)
+
+    result = s3.download_file('lambda-judge-tests-bucket', key, str(zip_path))
+    print('result:', result)
+
+    print('getting secrets...')
+    get_secret_value_response = secret_manager.get_secret_value(SecretId=encryption_secret_key_id)
+    print('got response:', get_secret_value_response)
+    secrets = json.loads(get_secret_value_response['SecretString'])
+    print('secrets:', secrets)
+    encryption_key = secrets['EFS_PROBLEMS_ENCRYPTION_KEY']
+    print('encryption_key', encryption_key)
+
+    zip_path.unlink(missing_ok=True)
+    return {
+        'status_code': 200
+    }
