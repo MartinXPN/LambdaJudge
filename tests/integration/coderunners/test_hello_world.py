d004292f36ab3da5f42c8f7de527d1494063ec28
MartinXPN <mirakyanmartin@gmail.com>
2022-05-12 14:47:44 +0400

    added hello world tests for all languages

---
+++ tests/integration/coderunners/test_hello_world.py
@@ -0,0 +1,48 @@
+from textwrap import dedent
+
+from bouncer.coderunners import CodeRunner
+from models import SubmissionRequest, TestCase, Status, SubmissionResult
+from tests.integration.config import lambda_client
+
+
+class TestHelloWorld:
+    test_cases = [TestCase(input='', target='Hello World!')]
+
+    @staticmethod
+    def run_test(request: SubmissionRequest) -> SubmissionResult:
+        res = CodeRunner.from_language(language=request.language).invoke(lambda_client, request=request)
+        print(res)
+        assert res.overall.status == Status.OK
+        assert len(res.test_results) == 1 and res.test_results[0].status == Status.OK
+        return res
+
+    def test_python(self):
+        self.run_test(SubmissionRequest(test_cases=self.test_cases, language='python', code={
+            'main.py': 'print("Hello World!")',
+        }))
+
+    def test_cpp(self):
+        self.run_test(SubmissionRequest(test_cases=self.test_cases, language='C++17', code={
+            'main.cpp': '#include <iostream>\nint main() { std::cout << "Hello World!"; return 0; }',
+        }))
+
+    def test_js(self):
+        self.run_test(SubmissionRequest(test_cases=self.test_cases, language='js', time_limit=5, code={
+            'main.js': 'console.log(\'Hello World!\')',
+        }))
+
+    def test_csharp(self):
+        self.run_test(SubmissionRequest(test_cases=self.test_cases, language='C#', time_limit=5, code={
+            'main.cs': 'System.Console.WriteLine("Hello World!");',
+        }))
+
+    def test_java(self):
+        self.run_test(SubmissionRequest(test_cases=self.test_cases, language='Java', time_limit=5, code={
+            'main.java': dedent('''
+                class Main {
+                    public static void main(String[] args) {
+                        System.out.println("Hello World!");
+                    }
+                }
+            ''')
+        }))
