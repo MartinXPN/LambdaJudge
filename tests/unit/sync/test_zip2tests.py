22e6d2c6337ae38dd43f0c6226bb26a233144ed7
MartinXPN <mirakyanmartin@gmail.com>
2024-01-26 21:48:40 +0400

    Added a more robust test case for subfolders

--- tests/unit/sync/test_zip2tests.py
+++ tests/unit/sync/test_zip2tests.py
@@ -35,18 +35,12 @@ class TestTruncation:
 
     def test_files(self):
         with TemporaryDirectory() as tests_dir:
-            with open(f'{tests_dir}/00.in.txt', 'w') as f:
-                f.write('First input')
-            with open(f'{tests_dir}/00.out.txt', 'w') as f:
-                f.write('First target')
-
-            with open(f'{tests_dir}/00.in.yoyo.txt', 'w') as f:   # We should have a `yoyo.txt` when the program starts
-                f.write('First file input')
-            with open(f'{tests_dir}/00.out.yoyo.txt', 'w') as f:  # We should have another `yoyo.txt` in the end
-                f.write('First file target')
-            with open(f'{tests_dir}/00.out.bik.txt', 'w') as f:
-                f.write('Second file target')
-
+            tests_dir = Path(tests_dir)
+            (tests_dir / '00.in.txt').write_text('First input')
+            (tests_dir / '00.out.txt').write_text('First target')
+            (tests_dir / '00.in.yoyo.txt').write_text('First file input')
+            (tests_dir / '00.out.yoyo.txt').write_text('First file target')
+            (tests_dir / '00.out.bik.txt').write_text('Second file target')
             shutil.make_archive(f'{tests_dir}/res', 'zip', tests_dir)
             tests = zip2tests(Path(tests_dir) / 'res.zip')
             test = tests[0]
@@ -59,33 +53,58 @@ class TestTruncation:
 
     def test_subfolders(self):
         with TemporaryDirectory() as tests_dir:
-            with open(f'{tests_dir}/00.in.txt', 'w') as f:
-                f.write('First input')
-            with open(f'{tests_dir}/00.out.txt', 'w') as f:
-                f.write('First target')
+            tests_dir = Path(tests_dir)
 
-            # subfolder/yoyo.txt in inputs and targets
-            Path(f'{tests_dir}/00.in.subfolder').mkdir(parents=True, exist_ok=True)
-            Path(f'{tests_dir}/00.out.subfolder/subsubfolder').mkdir(parents=True, exist_ok=True)
-            with open(f'{tests_dir}/00.in.subfolder/yoyo.txt', 'w') as f:
-                f.write('First file input')
-            with open(f'{tests_dir}/00.out.subfolder/yoyo.txt', 'w') as f:
-                f.write('First file target')
-            with open(f'{tests_dir}/00.out.subfolder/subsubfolder/hey.txt', 'w') as f:
-                f.write('Second subfolder target')
-            with open(f'{tests_dir}/00.out.bik.txt', 'w') as f:
-                f.write('Second file target')
+            # Test 1
+            (tests_dir / '01.in.txt').write_text('First input')
+            (tests_dir / '01.out.txt').write_text(
+                'subfolder1/subfolder2/subfolder3/image1.jpg\n'
+                'subfolder1/subfolder4/subfolder3/image2.jpg\n'
+                'subfolder2/subfolder2/image5.jpeg\n'
+                'image6.jpg'
+            )
+
+            Path(f'{tests_dir}/01.in.subfolder1/subfolder2/subfolder3').mkdir(parents=True, exist_ok=True)
+            Path(f'{tests_dir}/01.in.subfolder1/subfolder4/subfolder3').mkdir(parents=True, exist_ok=True)
+            Path(f'{tests_dir}/01.in.subfolder2/subfolder2').mkdir(parents=True, exist_ok=True)
+
+            (tests_dir / '01.in.subfolder1/subfolder2/subfolder3/image1.jpg').write_text('some jpeg file')
+            (tests_dir / '01.in.subfolder1/subfolder4/subfolder3/image2.jpg').write_text('some jpeg file')
+            (tests_dir / '01.in.subfolder2/subfolder2/image5.jpeg').write_text('some jpeg file')
+            (tests_dir / '01.in.image6.jpg').write_text('some jpeg file')
+
+            # Test 2
+            (tests_dir / '02.in.txt').write_text('Second input')
+            (tests_dir / '02.out.txt').write_text('lMJUev/mFDLKv/UHhhMCMwE.jpeg')
+
+            Path(f'{tests_dir}/02.in.lMJUev/mFDLKv').mkdir(parents=True, exist_ok=True)
+            (tests_dir / '02.in.lMJUev/mFDLKv/UHhhMCMwE.jpeg').write_text('some jpeg file')
 
             shutil.make_archive(f'{tests_dir}/res', 'zip', tests_dir)
             tests = zip2tests(Path(tests_dir) / 'res.zip')
+
+            # Check Test 1
             test = tests[0]
             print(test)
             assert test.input == 'First input'
-            assert test.target == 'First target'
-            assert test.input_files['subfolder/yoyo.txt'] == 'First file input'
-            assert test.target_files['subfolder/yoyo.txt'] == 'First file target'
-            assert test.target_files['subfolder/subsubfolder/hey.txt'] == 'Second subfolder target'
-            assert test.target_files['bik.txt'] == 'Second file target'
+            assert test.target == (
+                'subfolder1/subfolder2/subfolder3/image1.jpg\n'
+                'subfolder1/subfolder4/subfolder3/image2.jpg\n'
+                'subfolder2/subfolder2/image5.jpeg\n'
+                'image6.jpg'
+            )
+            assert test.input_files['subfolder1/subfolder2/subfolder3/image1.jpg'] == 'some jpeg file'
+            assert test.input_files['subfolder1/subfolder4/subfolder3/image2.jpg'] == 'some jpeg file'
+            assert test.input_files['subfolder2/subfolder2/image5.jpeg'] == 'some jpeg file'
+            assert test.input_files['image6.jpg'] == 'some jpeg file'
+
+            # Check Test 2
+            test = tests[1]
+            print(test)
+            assert test.input == 'Second input'
+            assert test.target == 'lMJUev/mFDLKv/UHhhMCMwE.jpeg'
+            assert test.input_files['lMJUev/mFDLKv/UHhhMCMwE.jpeg'] == 'some jpeg file'
+
 
     def test_assets(self):
         with TemporaryDirectory() as tests_dir:
