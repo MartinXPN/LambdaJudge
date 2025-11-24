11e1bf81e35808b923785a7fbeb9c8e206972ebd
MartinXPN <mirakyanmartin@gmail.com>
2025-11-24 13:19:19 +0400

    Proper handling for zip file

--- testgen/generator.py
+++ testgen/generator.py
@@ -1,6 +1,8 @@
+from pathlib import Path
+
 from models import TestGenRequest, TestGenResponse
 
 
-def generate_tests(request: TestGenRequest) -> TestGenResponse:
-    ...
-    return TestGenResponse(status='success', message='')
+def generate_tests(request: TestGenRequest) -> tuple[TestGenResponse, Path]:
+    zip_path = Path('/tmp/') / 'tests.zip'
+    return TestGenResponse(status='success', message=''), zip_path
--- testgen/generator_app.py
+++ testgen/generator_app.py
@@ -1,14 +1,12 @@
 import os
-from pathlib import Path
 
 import boto3
 from botocore.config import Config
 
-from models import TestGenRequest
+from models import TestGenRequest, TestGenResponse
 from testgen.generator import generate_tests
 
 TESTS_BUCKET = os.getenv('TESTS_BUCKET')
-ZIP_PATH = Path('/tmp/') / 'tests.zip'
 
 
 def handler(event, context):
@@ -26,10 +24,17 @@ def handler(event, context):
         raise ValueError('No credentials provided to upload tests')
 
     # Generate the test cases and compress them into a zip file
-    results = generate_tests(request)
+    results, zip_path = generate_tests(request)
     print(f'Done generating tests: {results}')
     if results.status == 'error':
         return results.to_json()
+
+    print(f'Zip path: {zip_path} => exists:{zip_path.exists()}, is_file:{zip_path.is_file()}')
+    if not zip_path.exists() or not zip_path.is_file():
+        return TestGenResponse(
+            status='error',
+            message='No zip file generated. It should be named `tests.zip`',
+        ).to_json()
 
     # Upload the results to S3
     s3 = boto3.client(
@@ -41,13 +46,13 @@ def handler(event, context):
     )
     print(f'Initialized the S3 client: {s3}')
 
-    # TODO: Implement the error handling (missing zip, S3 upload failure, etc.)
-    with open(ZIP_PATH, 'rb') as f:
-        s3.put_object(
-            Bucket=TESTS_BUCKET,
-            Key=f'{request.problem}.zip',
-            Body=f,
-            ServerSideEncryption='AES256',
-        )
+    # Upload the zip file to S3
+    s3.put_object(
+        Bucket=TESTS_BUCKET,
+        Key=f'{request.problem}.zip',
+        Body=zip_path.read_bytes(),
+        ServerSideEncryption='AES256',
+    )
+    print(f'Successfully uploaded tests archive to s3://{TESTS_BUCKET}/{request.problem}.zip')
 
     return results.to_json()
